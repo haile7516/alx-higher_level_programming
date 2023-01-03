0f822977162f952dae0840aaccaebabda68788bb
haile7516 <haile7516@gmail.com>
2023-01-03 10:23:49 -0800

    task8

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+ in range(0, 100):
+
+         if i != 99:
+
+                     print("{:02d}, ".format(i), end="")
+
+                         else:
+
+                                     print("{:02d}".format(i))
