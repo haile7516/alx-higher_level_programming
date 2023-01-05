aee5b06b6541afeef4844ad1b0c16eaaea82114a
haile7516 <haile7516@gmail.com>
2023-01-05 07:31:25 -0800

    test6

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+mport sys
+
+if __name__ == "__main__":
+
+        n = len(sys.argv)
+
+            sum = 0
+
+                for i in range(1, n):
+
+                            sum += int(sys.argv[i])
+
+                                print(sum)
+
+
