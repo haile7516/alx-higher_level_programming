301143ca7ed3ecdb5597e35acf82dd874d646a47
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-05 20:08:43 -0800

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,8 @@
-mport sys
-
+#!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-
-        n = len(sys.argv)
-
-            sum = 0
-
-                for i in range(1, n):
-
-                            sum += int(sys.argv[i])
-
-                                print(sum)
-
-
+    n = len(sys.argv)
+    sum = 0
+    for i in range(1, n):
+        sum += int(sys.argv[i])
+    print(sum)
