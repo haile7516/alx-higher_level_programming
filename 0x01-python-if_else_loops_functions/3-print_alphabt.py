ebf58894d6b58157cbabeda3d2b4475b2770c486
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-05 20:38:01 -0800

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
- in range(97, 123):
-
-         if i == 101 or i == 113:
-
-                     continue
-
-                     print("{:c}".format(i), end='')
+ #!/usr/bin/python3
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    print("{:c}".format(i), end='')
