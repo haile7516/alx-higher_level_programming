f4a7db6264b61aca4ed9359590068faabcc9f169
haile7516 <haile7516@gmail.com>
2023-01-05 21:10:59 -0800

    taskm

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+se(str):
+
+        for i in str:
+
+                    i = ord(i)
+
+                            if i >= 97 and i <= 122:
+
+                                            i -= 32
+
+                                                    i = chr(i)
+
+                                                            print("{}".format(i), end='')
+
+                                                                print("")
