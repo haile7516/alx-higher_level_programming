fa6f7d38ebc19652274400fe94a0ecb7ddfa942a
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-14 15:36:25 -0800

    Update 6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
-orted_dictionary(a_dictionary):
-
-        for key, value in sorted(a_dictionary.items()):
-
-                    print(key, value, sep=': ')
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    for key, value in sorted(a_dictionary.items()):
+        print(key, value, sep=': ')
