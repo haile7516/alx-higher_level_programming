8fa8eef762a28db42bb532ec7f4090e5cd068f64
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-14 15:29:37 -0800

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,7 @@
-q_add(my_list=[]):
-
-        new_list = set(my_list)
-
-            sum = 0
-
-                for i in new_list:
-
-                            sum += i
-
-                                return sum
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = set(my_list)
+    sum = 0
+    for i in new_list:
+        sum += i
+    return sum
