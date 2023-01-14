bbe448feb0074916654b24203dfe32315416ebbc
haile7516 <haile7516@gmail.com>
2023-01-14 15:03:58 -0800

    task1

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+q_add(my_list=[]):
+
+        new_list = set(my_list)
+
+            sum = 0
+
+                for i in new_list:
+
+                            sum += i
+
+                                return sum
