f4674a08512cea6a091a536dcbc3850c998289cc
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-14 15:26:55 -0800

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
-earch_replace(my_list, search, replace):
-
-        return [replace if value == search else value for value in my_list]
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    return [replace if value == search else value for value in my_list]
