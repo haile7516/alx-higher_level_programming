1dac82ba022b84c3b840f776ded3d120d45da89b
haile7516 <haile7516@gmail.com>
2023-01-14 15:58:48 -0800

    task

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+_dictionary, value):
+
+        for key, val in sorted(a_dictionary.items()):
+
+                    if val == value:
+
+                                    del a_dictionary[key]
+
+                                        return a_dictionary
