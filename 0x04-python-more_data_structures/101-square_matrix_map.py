32dbab0cb965256b4bc8c1bb7409865d4b191075
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-14 16:01:32 -0800

    Update 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
-quare_matrix_map(matrix=[]):
-
-        return list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    return list(map(lambda y: list(map(lambda x: x ** 2, y)), matrix))
