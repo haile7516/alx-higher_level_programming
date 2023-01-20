3184fd601cb5b89d777fe74f835f6ba5874c71f6
haile7516 <haile7516@gmail.com>
2023-01-20 09:53:09 -0800

    ty

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,19 @@
+n_list(my_list, idx, element):
+
+        n = len(my_list)
+
+            list_cpy = my_list.copy()
+
+                if idx < 0:
+
+                            return list_cpy
+
+                            elif idx > n - 1:
+
+                                        return list_cpy
+
+                                        else:
+
+                                                    list_cpy[idx] = element
+
+                                                            return 
