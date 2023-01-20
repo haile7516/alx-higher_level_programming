44dbe501fe85f1cfcbb75082bf7f9917387311bd
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-20 10:23:33 -0800

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
-
-st_integer(my_list=[]):
-
-        for list in my_list:
-
-                    print('{:d}'.format(list))
-
-
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for list in my_list:
+        print('{:d}'.format(list))
