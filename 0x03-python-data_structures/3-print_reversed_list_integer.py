b82a9a897bdc3e53614a74a7e51374d1237315da
haile7516 <haile7516@gmail.com>
2023-01-20 09:51:09 -0800

    tw

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+st_integer(my_list=[]):
+
+        if not my_list:
+
+                    return None
+
+                    my_list.reverse()
+
+                        for i in my_list:
+
+                                    print('{:d}'.format(i))
