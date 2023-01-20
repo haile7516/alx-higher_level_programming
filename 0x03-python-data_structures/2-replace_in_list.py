704363affdb8f1fa3e9a1a60b730568dee142f48
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-20 10:34:38 -0800

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,18 +1,11 @@
+#!/usr/bin/python3
 
-ce_in_list(my_list, idx, element):
-
-        n = len(my_list)
-
-            if idx < 0:
-
-                        return my_list
-
-                        elif idx > n - 1:
-
-                                    return my_list
-
-                                    else:
-
-                                                my_list[idx] = element
-
-                                                        return 
+def replace_in_list(my_list, idx, element):
+    n = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx > n - 1:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
