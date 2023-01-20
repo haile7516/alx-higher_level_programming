6ce9cc337fd97374554813dde81a536fb3d45ec9
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-20 10:26:42 -0800

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,13 +1,8 @@
-t(my_list, idx):
-
-        if idx < 0:
-
-                    return None
-
-                    if idx >= len(my_list):
-
-                                return None
-
-                                else:
-
-                                            return my_list[idx]
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
