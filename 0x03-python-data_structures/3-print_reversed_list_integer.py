ec574e9a055885e26a33fa857d2fdccbf868989f
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-01-20 10:37:12 -0800

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,11 +1,7 @@
-st_integer(my_list=[]):
-
-        if not my_list:
-
-                    return None
-
-                    my_list.reverse()
-
-                        for i in my_list:
-
-                                    print('{:d}'.format(i))
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return None
+    my_list.reverse()
+    for i in my_list:
+        print('{:d}'.format(i))
