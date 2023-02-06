dcab48601a7fa8918ed43792c44906a526ca9c2d
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-02-06 11:55:26 -0800

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,19 +1,11 @@
-afe_print_division(a, b):
+#!/usr/bin/python3
+def safe_print_division(a, b):
 
+    try:
+        response = a / b
+    except ZeroDivisionError:
+        response = None
+    finally:
+        print('Inside result: {}'.format(response))
 
-
-        try:
-
-                    response = a / b
-
-                        except ZeroDivisionError:
-
-                                    response = None
-
-                                        finally:
-
-                                                    print('Inside result: {}'.format(response))
-
-
-
-                                                        return response
+    return response
