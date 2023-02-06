6ce224092980b7e534def0e3096d501fd001f406
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-02-06 12:00:38 -0800

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,21 +1,12 @@
-mport sys
-
-
-
+#!/usr/bin/python3
+import sys
 
 
 def safe_print_integer_err(value):
+    try:
+        print('{:d}'.format(value))
+    except Exception as error:
+        print('Exception: {}'.format(error), file=sys.stderr)
+        return False
 
-        try:
-
-                    print('{:d}'.format(value))
-
-                        except Exception as error:
-
-                                    print('Exception: {}'.format(error), file=sys.stderr)
-
-                                            return False
-
-
-
-                                            return True
+    return True
