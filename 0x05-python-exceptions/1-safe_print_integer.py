bc68785918d027ccea2bdf2718ca2978e71ce581
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-02-06 11:52:46 -0800

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,9 @@
-afe_print_integer(value):
+#!/usr/bin/python3
+def safe_print_integer(value):
 
-
-
-        try:
-
-                    print('{:d}'.format(value))
-
-                        except Exception as error:
-
-                                    return False
-
-                                    else:
-
-                                                return True
+    try:
+        print('{:d}'.format(value))
+    except Exception as error:
+        return False
+    else:
+        return True
