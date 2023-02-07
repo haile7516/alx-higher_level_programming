3e50ec6185c4ae35933d6908807036d1f9f005b8
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-02-07 11:16:58 -0800

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,30 +1,18 @@
-
-dule that contains a function that writes to a text file
-
+#!/usr/bin/python3
+""" Module that contains a function that writes to a text file
 """
 
 
-
-
-
 def write_file(filename="", text=""):
+    """ Function that writes to a text file
+    Args:
+        filename: filename
+        text: text to write
+    Raises
+        Exception: when the file can be opened
+    """
 
-        """ Function that writes to a text file
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
 
-            Args:
-
-                        filename: filename
-
-                                text: text to write
-
-                                    Raises
-
-                                            Exception: when the file can be opened
-
-                                                """
-
-
-
-                                                    with open(filename, 'w', encoding="utf-8") as f:
-
-                                                            return f.write(text)
+                                           
