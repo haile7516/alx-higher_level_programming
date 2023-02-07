567b098fbb7e2f7165b6adb143a559207b4657db
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-02-07 11:15:39 -0800

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,28 +1,15 @@
-
-dule that contains a function that reads from a file """
-
-
-
+#!/usr/bin/python3
+""" Module that contains a function that reads from a file """
 
 
 def read_file(filename=""):
+    """ Function that reads from a file
+    Args:
+        filename: filename
+    Raises
+        Exception: when the file can be opened
+    """
 
-        """ Function that reads from a file
-
-            Args:
-
-                        filename: filename
-
-                            Raises
-
-                                    Exception: when the file can be opened
-
-                                        """
-
-
-
-                                            with open(filename, 'r', encoding="utf-8") as f:
-
-                                                    read_data = f.read()
-
-                                                            print(read_data, end='')
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
