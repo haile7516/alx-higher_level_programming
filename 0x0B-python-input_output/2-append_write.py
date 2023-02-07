ea64576ca4cd2b2bb88895651bb0618acd7885bb
HAILEMARIAM GEBREMICHAEL <104761075+haile7516@users.noreply.github.com>
2023-02-07 11:21:08 -0800

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,30 +1,16 @@
-
-dule that contains a function that appends to a text file
-
+#!/usr/bin/python3
+""" Module that contains a function that appends to a text file
 """
 
 
-
-
-
 def append_write(filename="", text=""):
+    """ Function that appends to a text file
+    Args:
+        filename: filename
+        text: text to write
+    Raises
+        Exception: when the file can be opened
+    """
 
-        """ Function that appends to a text file
-
-            Args:
-
-                        filename: filename
-
-                                text: text to write
-
-                                    Raises
-
-                                            Exception: when the file can be opened
-
-                                                """
-
-
-
-                                                    with open(filename, 'a', encoding="utf-8") as f:
-
-                                                            return f.write(text)
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
